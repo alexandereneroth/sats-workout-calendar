537261e4073465a00d7a935fbb314c6c5c485e26
Patrik Appelqvist <Appelqvist.patrik@gmail.com>
2015-05-04 12:57:00 +0200

    Code re-structuring, moved out some code to private helper methods.

--- app/src/main/java/se/greatbrain/sats/fragment/ClassDetailFragment.java
+++ app/src/main/java/se/greatbrain/sats/fragment/ClassDetailFragment.java
@@ -22,7 +22,8 @@ import se.greatbrain.sats.R;
 import se.greatbrain.sats.event.ClassDetailEvent;
 import se.greatbrain.sats.util.DateUtil;
 
-public class ClassDetailFragment extends Fragment implements YouTubeThumbnailView.OnInitializedListener
+public class ClassDetailFragment extends Fragment implements YouTubeThumbnailView
+        .OnInitializedListener
 {
     private ActivityWrapper wrapper;
     private String videoId;
@@ -45,38 +46,6 @@ public class ClassDetailFragment extends Fragment implements YouTubeThumbnailVie
         TextView classDate = (TextView) view.findViewById(R.id.class_detail_date_answer);
         TextView instructorName = (TextView) view.findViewById(R.id.class_detail_instructor_answer);
 
-        ProgressBar condition = (ProgressBar) view.findViewById(R.id
-                .class_detail_condition_progress_bar);
-        ProgressBar strength = (ProgressBar) view.findViewById(R.id
-                .class_detail_strength_progress_bar);
-        ProgressBar flexibility = (ProgressBar) view.findViewById(R.id
-                .class_detail_flexibility_progress_bar);
-        ProgressBar balance = (ProgressBar) view.findViewById(R.id
-                .class_detail_balance_progress_bar);
-        ProgressBar agility = (ProgressBar) view.findViewById(R.id
-                .class_detail_agility_progress_bar);
-
-        YouTubeThumbnailView thumbnail = (YouTubeThumbnailView) view.findViewById(R.id
-                .class_detail_youtube_video);
-        thumbnail.initialize(API_KEY, this);
-
-        final String videoUrl = wrapper.trainingActivity.getClassType().getVideoUrl();
-        int questionMarkPosition = videoUrl.indexOf("?");
-        videoId = videoUrl.substring(questionMarkPosition - 11, questionMarkPosition);
-
-        Log.d("Url", videoUrl);
-        Log.d("VideoId", videoId);
-
-        thumbnail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                Intent intent = YouTubeIntents.createPlayVideoIntentWithOptions(getActivity(),
-                        videoId, false, false);
-                startActivity(intent);
-            }
-        });
-
         className.setText(wrapper.trainingActivity.getClassType().getName());
         classDuration.setText(String.valueOf(wrapper.trainingActivity.getDurationInMinutes()) +
                 "min");
@@ -92,18 +61,8 @@ public class ClassDetailFragment extends Fragment implements YouTubeThumbnailVie
         instructorName.setText(
                 wrapper.trainingActivity.getBooking().getSatsClass().getInstructorId());
 
-        int conditionValue = wrapper.trainingActivity.getClassType().getProfile().get(0).getValue();
-        int strengthValue = wrapper.trainingActivity.getClassType().getProfile().get(1).getValue();
-        int flexibilityValue = wrapper.trainingActivity.getClassType().getProfile().get(2)
-                .getValue();
-        int balanceValue = wrapper.trainingActivity.getClassType().getProfile().get(3).getValue();
-        int agilityValue = wrapper.trainingActivity.getClassType().getProfile().get(4).getValue();
-
-        condition.setProgress(conditionValue);
-        strength.setProgress(strengthValue);
-        flexibility.setProgress(flexibilityValue);
-        balance.setProgress(balanceValue);
-        agility.setProgress(agilityValue);
+        setUpProgressBar(view);
+        setUpYoutubeThumbNail(view);
 
         return view;
     }
@@ -125,7 +84,8 @@ public class ClassDetailFragment extends Fragment implements YouTubeThumbnailVie
             YouTubeThumbnailLoader youTubeThumbnailLoader)
     {
         youTubeThumbnailLoader.setOnThumbnailLoadedListener(new YouTubeThumbnailLoader
-                .OnThumbnailLoadedListener() {
+                .OnThumbnailLoadedListener()
+        {
             @Override
             public void onThumbnailLoaded(YouTubeThumbnailView youTubeThumbnailView, String s)
             {
@@ -135,8 +95,6 @@ public class ClassDetailFragment extends Fragment implements YouTubeThumbnailVie
             public void onThumbnailError(YouTubeThumbnailView youTubeThumbnailView,
                     YouTubeThumbnailLoader.ErrorReason errorReason)
             {
-                Toast.makeText(getActivity(), "ThumbnailLoadedListener.onThumbnailError()",
-                        Toast.LENGTH_LONG).show();
             }
         });
 
@@ -146,8 +104,58 @@ public class ClassDetailFragment extends Fragment implements YouTubeThumbnailVie
     @Override
     public void onInitializationFailure(YouTubeThumbnailView youTubeThumbnailView,
             YouTubeInitializationResult youTubeInitializationResult)
+    {}
+
+    private void setUpProgressBar(View view)
     {
-        Toast.makeText(getActivity(), "YouTubeThumbnailView.onInitializationFailure()",
-                Toast.LENGTH_LONG).show();
+        ProgressBar condition = (ProgressBar) view.findViewById(R.id
+                .class_detail_condition_progress_bar);
+        ProgressBar strength = (ProgressBar) view.findViewById(R.id
+                .class_detail_strength_progress_bar);
+        ProgressBar flexibility = (ProgressBar) view.findViewById(R.id
+                .class_detail_flexibility_progress_bar);
+        ProgressBar balance = (ProgressBar) view.findViewById(R.id
+                .class_detail_balance_progress_bar);
+        ProgressBar agility = (ProgressBar) view.findViewById(R.id
+                .class_detail_agility_progress_bar);
+
+        int conditionValue = wrapper.trainingActivity.getClassType().getProfile().get(0).getValue();
+        int strengthValue = wrapper.trainingActivity.getClassType().getProfile().get(1).getValue();
+        int flexibilityValue = wrapper.trainingActivity.getClassType().getProfile().get(2)
+                .getValue();
+        int balanceValue = wrapper.trainingActivity.getClassType().getProfile().get(3).getValue();
+        int agilityValue = wrapper.trainingActivity.getClassType().getProfile().get(4).getValue();
+
+        condition.setProgress(conditionValue);
+        strength.setProgress(strengthValue);
+        flexibility.setProgress(flexibilityValue);
+        balance.setProgress(balanceValue);
+        agility.setProgress(agilityValue);
+    }
+
+
+    private void setUpYoutubeThumbNail(View view)
+    {
+        YouTubeThumbnailView thumbnail = (YouTubeThumbnailView) view.findViewById(R.id
+                .class_detail_youtube_video);
+        thumbnail.initialize(API_KEY, this);
+
+        final String videoUrl = wrapper.trainingActivity.getClassType().getVideoUrl();
+        int questionMarkPosition = videoUrl.indexOf("?");
+        videoId = videoUrl.substring(questionMarkPosition - 11, questionMarkPosition);
+
+        Log.d("Url", videoUrl);
+        Log.d("VideoId", videoId);
+
+        thumbnail.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = YouTubeIntents.createPlayVideoIntentWithOptions(getActivity(),
+                        videoId, false, false);
+                startActivity(intent);
+            }
+        });
     }
 }
