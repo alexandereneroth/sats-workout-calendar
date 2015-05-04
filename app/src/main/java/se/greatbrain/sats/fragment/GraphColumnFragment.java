78e528de22a88d3dadd0bfe2852e48590d904fce
alexander-eneroth <89alexius@gmail.com>
2015-05-04 15:57:19 +0200

    Styles and layouts the calendar(the graph)

--- app/src/main/java/se/greatbrain/sats/fragment/GraphColumnFragment.java
+++ app/src/main/java/se/greatbrain/sats/fragment/GraphColumnFragment.java
@@ -3,9 +3,12 @@ package se.greatbrain.sats.fragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -19,92 +22,98 @@ public class GraphColumnFragment extends Fragment
 
     public interface OnPageClickedListener
     {
-        void onPageClicked (int page);
+        void onPageClicked(int page);
     }
 
     private static final String TAG = "ScreenSlidePageFragment";
 
     @Override
-    public void onAttach (Activity activity)
+    public void onAttach(Activity activity)
     {
-        super.onAttach( activity );
+        super.onAttach(activity);
 
         try
         {
             listenerOnPageClicked_MainActivity = (OnPageClickedListener) activity;
-        } catch (ClassCastException e)
+        }
+        catch (ClassCastException e)
         {
-            throw new ClassCastException( activity.toString()
-                    + " must implement" + OnPageClickedListener.class.getName() );
+            throw new ClassCastException(activity.toString()
+                    + " must implement" + OnPageClickedListener.class.getName());
         }
     }
 
     @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState)
     {
-        RelativeLayout rootView = (RelativeLayout) inflater.inflate( R.layout
+        LinearLayout rootView = (LinearLayout) inflater.inflate(R.layout
                         .fragment_graph_column,
-                container, false );
-        final int myAdapterIndex = getArguments().getInt( GraphFragment.ScreenSlidePagerAdapter
-                .ITEM_INDEX );
+                container, false);
+        final int myAdapterIndex = getArguments().getInt(GraphFragment.ScreenSlidePagerAdapter
+                .ITEM_INDEX);
 
         if (myAdapterIndex % 2 == 0)
         {
-            rootView.setBackgroundColor( getResources().getColor( R.color.gray_dark ) );
+            rootView.setBackgroundColor(getResources().getColor(R.color.primary_calendar));
+        }
+        else
+        {
+            rootView.setBackgroundColor(getResources().getColor(R.color.secondary_calendar));
         }
 
-        rootView.setOnClickListener( new View.OnClickListener()
+        rootView.setOnClickListener(new View.OnClickListener()
         {
 
             @Override
-            public void onClick (View v)
+            public void onClick(View v)
             {
-                listenerOnPageClicked_MainActivity.onPageClicked( myAdapterIndex );
+                listenerOnPageClicked_MainActivity.onPageClicked(myAdapterIndex);
             }
-        } );
+        });
 
-        int idToPlaceNextViewBelow = addRows( rootView, 4 );
+        int idToPlaceNextViewBelow = addRows(rootView, 4);
 
         addDateRow(idToPlaceNextViewBelow, rootView);
         return rootView;
     }
 
 
-    private int addRows (RelativeLayout rootView, int rows)
+    private int addRows(LinearLayout rootView, int rows)
     {
         int idToPlaceNextViewBelow = R.id.graph_week_text;
         for (int i = 0; i < rows; ++i)
         {
-            TextView row = new TextView( rootView.getContext() );
-            row.setText( "Row" + i );
-            row.setBackground(getResources().getDrawable(R.drawable.graph_column_bg));
-            row.setTextColor( getResources().getColor( R.color.abc_primary_text_material_light ) );
+            TextView row = new TextView(rootView.getContext());
+            row.setText(String.valueOf(i));
+            row.setTextColor(getResources().getColor(R.color.white));
+            row.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
 
-            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams( ViewGroup.LayoutParams
-                    .MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT );
-            params.addRule( RelativeLayout.BELOW, idToPlaceNextViewBelow );
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
             int id = i + 1;
-            row.setId( id );
+            row.setId(id);
             idToPlaceNextViewBelow = id;
+            params.weight = 1;
 
-            rootView.addView( row, params );
+            rootView.addView(row, params);
         }
         return idToPlaceNextViewBelow;
     }
 
-    private void addDateRow (int idToPlaceBelow, RelativeLayout rootView)
+    private void addDateRow(int idToPlaceBelow, LinearLayout rootView)
     {
-        TextView row = new TextView( rootView.getContext() );
+        TextView row = new TextView(rootView.getContext());
         row.setText("12-14/3");
-        row.setBackgroundColor( getResources().getColor( R.color
-                .bright_foreground_disabled_material_dark ) );
-        row.setTextColor( getResources().getColor( R.color.material_deep_teal_200 ) );
+        row.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);
+        row.setBackground(getResources().getDrawable(R.drawable.graph_column_bg));
+        row.setPadding(0,17,0,0);
+        row.setGravity(Gravity.CENTER | Gravity.TOP);
 
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams( ViewGroup.LayoutParams
-                .MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT );
-        params.addRule( RelativeLayout.BELOW, idToPlaceBelow);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams
+                .MATCH_PARENT, 100);//TODO check what ___ 100 is. dp? sp? in? px?
 
-        rootView.addView( row, params );
+        rootView.addView(row, params);
     }
 }
