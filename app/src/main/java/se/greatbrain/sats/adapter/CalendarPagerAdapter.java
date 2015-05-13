8cdc4b138d00e6291463ee24c3675077c06d601d
Patrik Appelqvist <Appelqvist.patrik@gmail.com>
2015-05-13 13:06:27 +0200

    Removed the exception that was thrown in CalendarPagerAdapter. Caused crash on startup

--- app/src/main/java/se/greatbrain/sats/adapter/CalendarPagerAdapter.java
+++ app/src/main/java/se/greatbrain/sats/adapter/CalendarPagerAdapter.java
@@ -101,7 +101,6 @@ public class CalendarPagerAdapter extends FragmentStatePagerAdapter
                 return i;
             }
         }
-        //throw new IllegalStateException("Current week is not in dates");
 
         return 0;
     }
